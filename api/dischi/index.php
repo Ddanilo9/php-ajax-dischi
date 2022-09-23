a15ba04950f598164500a0542b3927604e8bf647
Ddanilo9 <interiordanilo3@gmail.com>
2022-09-23 16:38:09 +0200

    filter generi

--- api/dischi/index.php
+++ api/dischi/index.php
@@ -1,8 +1,27 @@
 <?php
+//dati
 include __DIR__.'/../../dischi/dischi.php';
+
+//recupero parametro Get
+$genr = isset($_GET['genre']) ? $_GET['genre'] : null;
+
+
+//filtrare in base al genere
+if($genr) {
+    $dischi = array_filter($dischi, function($d) use ($genr){
+        // $genr = $_GET['genre'];
+        return $genr == $d['genre'];
+    });
+}
+
+//set content type
 header('Content-Type: application/json');
+
+//formattare la risposta
 $data = [
     'succes' => true,
     'response' => $dischi
 ];
+
+//stampo in Json
 echo json_encode($data);
--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const app = new Vue({
     data: {
         title:'Vue Dischi',
         dischi: [],
+        genrs:['Rock', 'Pop', 'Metal', 'Jazz'],
+        selectedGenrs: '',
     },
     methods: {
         fetchDischi() {
-            axios.get('http://localhost/php-ajax-dischi/api/dischi/')
+            axios
+            .get('http://localhost/php-ajax-dischi/api/dischi/',{
+                params:{
+                    genre: this.selectedGenrs,
+                },
+            })
             .then((res) => {
                 console.log(res)
                 this.dischi = res.data.response
